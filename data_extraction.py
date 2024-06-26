694885d6e8e652013e49ceab0c0339cc22d1d7e7
Christian-Ray D. Garcia <47110238+Christian-rayGarcia@users.noreply.github.com>
2024-06-26 15:41:02 +0100

    Appending main file

--- data_extraction.py
+++ data_extraction.py
@@ -40,18 +40,3 @@ class DataExtraction:
 				writer.writerow(row.values())
 
 
-def process_pokemon_data(pokemon_base_info):
-	pokemon_type = [result['type']['name'] for result in pokemon_base_info['types']]
-	return {
-		"Index": pokemon_base_info["id"],
-		"Name": pokemon_base_info["name"],
-		"Type": " ".join(pokemon_type)
-	}
-
-
-base_url = "https://pokeapi.co/api/v2/"
-endpoint = "pokemon"
-output_file = "pokemon_base_info.csv"
-
-fetcher = DataExtraction(base_url, endpoint, process_pokemon_data, output_file)
-fetcher.get_data()
